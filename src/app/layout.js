74bdb35a0c3ce0c174b096bbe6caa244f5825e43
gayatrithakre2004 <gayatrithakre2004@gmail.com>
2024-11-22 14:05:46 +0530

    update hero

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,9 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider
-    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+   
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -30,6 +27,5 @@ export default function RootLayout({ children }) {
           {children}
         </body>
       </html>
-    </ClerkProvider>
   );
 }
